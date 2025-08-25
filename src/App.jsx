b852301cd556cb8951a3c0e9852620b350431b68
milliondreamsblog <akshatsan23@gmail.com>
2025-08-25 10:20:06 +0530

    you are live

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Homepage from './pages/Homepage'
+import { Routes, Route, Link } from "react-router-dom";
+import News from './pages/News&Insight'
+import ContactUs from './pages/Contactus'
+
+
+function App() {
+
+  return (
+    <div>
+          <Routes>
+            <Route path="/" element={<Homepage />} />
+            <Route path="/news" element={<News />} />
+            <Route path="/contact" element={<ContactUs />} />
+          </Routes>
+    </div>
+  )
+}
+
+export default App
