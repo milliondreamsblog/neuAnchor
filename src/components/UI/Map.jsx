ad2e0cfdcccf8de950212509ec563e5c3ddbe8e6
milliondreamsblog <akshatsan23@gmail.com>
2025-09-01 15:13:10 +0530

    Frontend upadte

--- src/components/UI/Map.jsx
+++ src/components/UI/Map.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
-import "Leaflet/dist/leaflet.css"
+import "leaflet/dist/leaflet.css"
 
 const MyMap = () => {
   const position = [51.505, -0.09]; // Example coordinates
